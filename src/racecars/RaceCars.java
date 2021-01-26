0a7897f784474699e50e279b5236d002b0160c91
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-01-27 00:26:02 +0200

    Intermittent results displayed and final results sorted descending.

--- src/racecars/RaceCars.java
+++ src/racecars/RaceCars.java
@@ -19,7 +19,39 @@ public class RaceCars {
                     intermittentLead = i + 1;
                     intermittentLeadDistance = cars[i];
                 }
+                if (cars[i] >= intermittentInterval) {
+                    printIntermittent = true;
+                    intermittentInterval += 100;
+                }
             }
+            if (printIntermittent) {
+                for (int i = 0; i < cars.length; i++) {
+                    System.out.print(cars[i] + "\t");
+                }
+                System.out.println("");
+                System.out.println("Race car no. " + intermittentLead + " is leading the race, at " + intermittentLeadDistance + "km covered.");
+            }
+            for (int i = 0; i < cars.length; i++) {
+                if (cars[i] >= 1000) {
+                    doRace = false;
+                    System.out.println("We have a winner!");
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < cars.length-1; i++) {
+            for (int j = i + 1; j < cars.length; j++) {
+                if (cars[i] < cars[j]) {
+                    int temp = cars[i];
+                    cars[i] = cars[j];
+                    cars[j] = temp;
+                }
+            }
+        }
+        System.out.println("Final results: ");
+        for (int i = 0; i < cars.length; i++) {
+            System.out.print(cars[i] + " ");
+            
         }
     }
     
