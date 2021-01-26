0111da39ef821742daf24781d2dc55dd42bca388
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-01-27 01:23:06 +0200

    Sports car class.

--- src/racecars/SportsCar.java
+++ src/racecars/SportsCar.java
@@ -1,46 +1,32 @@
 
 package racecars;
 
-public class SportsCar {
+public class SportsCar extends RegularCar {
+    private boolean spoiler;
     
+    public SportsCar(String name, int maxSpeed) {
+        super(name, maxSpeed);
+        this.spoiler = false;
+    }
+    
+    public void spoilerRelease() {
+        this.spoiler = !this.spoiler;
+    }
+    public void hitAccelerator(int howMuch) {
+        if(!this.spoiler) {
+            super.hitAccelerator(howMuch * 2);
+        } else {
+            super.hitAccelerator(howMuch);
+        }
+    }
+    public void hitBreaks(int howMuch) {
+        if (this.spoiler) {
+            super.hitBreaks(howMuch * 2); 
+        } else {
+            super.hitBreaks(howMuch);
+        }
+    }
+    public String toString() {
+        return "Sports car " + super.toString();
+    }
 }
-
-//        int[] cars = new int[8];
-//        int goMifune = cars[0];
-//        int kurioMifune = cars[1];
-//        int daisukeMifune = cars[2];
-//        int ayaMifune = cars[3];
-//        int fukumenRacer = cars[4];
-//        int takumiFujiwara = cars[5];
-//        int ryosukeTakahashi = cars[6];
-//        int keisukeTakahashi = cars[7];
-//
-//        int[] carLineup = new int [10];
-//        int checkpointCount = 10;
-//        int checkpoint = 100;
-//        int finishLine = 1000;
-//        int[][] raceStats = new int[carLineup.length][cars.length];
-//        int burst = (int) (Math.random() * 10 + 1); // kiekvieno ciklo metu
-//
-//        for (int i = 0; i < checkpointCount; i++) {
-//            for (int j = 0; j <= checkpoint; j++) {
-//                for (int r = 0; r < cars.length; r++) {
-//                    cars[r] += burst;
-//                    if (cars[r] >= checkpoint) {
-//                        continue;
-////                        break;
-//                    }
-//                    System.out.print(cars[r] + " ");
-//                    System.out.println("");
-//                }
-//            }
-//
-//        }
-//
-//        for (int i = 0; i < raceStats.length; i++) {
-//            carLineup = raceStats[i];
-//            for (int j = 0; j < cars.length; j++) {
-//                int racer = carLineup[j];
-//                System.out.println(racer + "; ");
-//            }
-//        }
